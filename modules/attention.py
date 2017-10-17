b2af8d82f53545415b72e2ab86f59b38a2390662
hengyuan-hu <kevin.huhy@gmail.com>
2017-10-17 15:34:20 -0400

    small modifications.

--- modules/attention.py
+++ modules/attention.py
@@ -1,42 +1,37 @@
 import torch.nn as nn
 from gated_tanh import GatedTanh
 
+
 class Attention(nn.Module):
   '''
   Generic attention modules that computes a soft attention distribution
   over a set of objects.
   '''
 
-  def __init__(self, num_objects, in_len, hidden_len):
+  def __init__(self, num_objects, in_dim, hidden_dim):
     super(Attention, self).__init__()
 
-    self.nonlinear = GatedTanh(in_len, hidden_len)
-    self.linear = nn.Linear(hidden_len, 1)
-    self.softmax = nn.Softmax()
-
+    self.nonlinear = GatedTanh(in_dim, hidden_dim)
+    self.linear = nn.Linear(hidden_dim, 1)
 
   def forward(self, inputs, objects):
     '''
     inputs    Input vector x_i is used to compute weight a_i for object o_i.
-              Dimensions: batch_size x num_objects x in_len
+              Dimensions: batch_size x num_objects x in_dim
 
     objects   Final output is sum(a_i * o_i) for all objects o_i
-              Dimensions: batch_size x num_objects x obj_len
+              Dimensions: batch_size x num_objects x obj_dim
     '''
-
-    batch_size = inputs.size(0)
-    num_objects = inputs.size(1)
-    in_len = inputs.size(2)
+    assert inputs.size()[:2] == objects.size()[:2]
+    batch_size, num_objects, _ = inputs.size()
 
     inputs = inputs.view(batch_size * num_objects, -1)
     x = self.nonlinear(inputs)
     x = self.linear(x)
+    x = x.view(batch_size, num_objects)
 
-    x = x.view(-1, num_objects)
-    weights = self.softmax(x)
-    weights = weights.unsqueeze(2)
-    weights = weights.expand(weights.size(0), weights.size(1), objects.size(2))
+    weights = nn.functional.softmax(x)
+    weights = weights.unsqueeze(2).expand_as(objects)
 
     out = objects * weights
     return out
-
--- modules/gated_tanh.py
+++ modules/gated_tanh.py
@@ -1,19 +1,15 @@
 import torch.nn as nn
 
-class GatedTanh(nn.Module):
 
+class GatedTanh(nn.Module):
   def __init__(self, in_dim, out_dim):
     super(GatedTanh, self).__init__()
 
     self.sigmoid_linear = nn.Linear(in_dim, out_dim)
-    self.sigmoid = nn.Sigmoid()
-
     self.tanh_linear = nn.Linear(in_dim, out_dim)
-    self.tanh = nn.Tanh()
 
   def forward(self, x):
-    tanh = self.tanh(self.tanh_linear(x))
-    sigmoid = self.sigmoid(self.sigmoid_linear(x))
+    tanh = nn.functional.tanh(self.tanh_linear(x))
+    sigmoid = nn.functional.sigmoid(self.sigmoid_linear(x))
     out = tanh * sigmoid
     return out
-
