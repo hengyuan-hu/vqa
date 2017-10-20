6855fb1309bbcd68c0478e1d59f70c371513cfe8
hengyuan-hu <kevin.huhy@gmail.com>
2017-10-20 17:10:28 -0400

    remove sth.

--- main.py
+++ main.py
@@ -15,28 +15,17 @@ import utils
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    # parser.add_argument('--dataset', type=str, required=True,
-    #                     help='dataset to use, (PTB, VQA)')
     # parser.add_argument('--model', type=str, default='LSTM',
     #                     help='type of recurrent net (LSTM, GRU)')
-    # parser.add_argument('--emsize', type=int, default=200,
-    #                     help='size of word embeddings')
     # parser.add_argument('--nhid', type=int, default=200,
     #                     help='# hidden units per layer')
     # parser.add_argument('--nlayers', type=int, default=2)
     # parser.add_argument('--lr', type=float, default=20)
-    # parser.add_argument('--clip', type=float, default=0.25, help='max gradient norm')
-    # parser.add_argument('--epochs', type=int, default=40)
+    parser.add_argument('--epochs', type=int, default=40)
     # parser.add_argument('--batch_size', type=int, default=20)
-    # parser.add_argument('--bptt', type=int, default=35, help='sequence length')
     # parser.add_argument('--dropout', type=float, default=0.2,
     #                     help='dropout applied to layers (0 = no dropout)')
-    # parser.add_argument('--tied', action='store_true',
-    #                     help='tie the word embedding and softmax weights')
-    parser.add_argument('--seed', type=int, default=1111,
-                        help='random seed')
-    # parser.add_argument('--save', type=str,  default='model.pt',
-    #                     help='path to save the final model')
+    parser.add_argument('--seed', type=int, default=1111, help='random seed')
     args = parser.parse_args()
     return args
 
