e3c22aaa07384eb30ec41f68b45fea13a9151c53
hengyuan-hu <kevin.huhy@gmail.com>
2017-10-21 01:37:23 -0400

    wn.

--- modules/classifier.py
+++ modules/classifier.py
@@ -1,12 +1,16 @@
 import torch
 import torch.nn as nn
+from torch.nn.utils.weight_norm import weight_norm
 from gated_tanh import GatedTanh
 
 
 class SimpleClassifier(nn.Module):
     def __init__(self, in_dim, hid_dim, out_dim):
         super(SimpleClassifier, self).__init__()
-        layers = [GatedTanh(in_dim, hid_dim), nn.Linear(hid_dim, out_dim)]
+        layers = [
+            GatedTanh(in_dim, hid_dim),
+            weight_norm(nn.Linear(hid_dim, out_dim), dim=None)
+        ]
         self.main = nn.Sequential(*layers)
 
     def forward(self, x):
