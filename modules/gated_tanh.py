077700670d5c426d1230a943eb94fab280d1eb1f
hengyuan-hu <kevin.huhy@gmail.com>
2017-10-22 11:49:41 -0400

    gated linear.

--- modules/gated_tanh.py
+++ modules/gated_tanh.py
@@ -6,15 +6,15 @@ class GatedTanh(nn.Module):
   def __init__(self, in_dim, out_dim):
     super(GatedTanh, self).__init__()
 
-    # self.sigmoid_linear = nn.Linear(in_dim, out_dim)
+    self.sigmoid_linear = weight_norm(nn.Linear(in_dim, out_dim), dim=None)
     self.tanh_linear = weight_norm(nn.Linear(in_dim, out_dim), dim=None)
 
   def forward(self, x):
-    out = nn.functional.relu(self.tanh_linear(x))
+    # out = nn.functional.relu(self.tanh_linear(x))
     # out = nn.functional.elu(self.tanh_linear(x))
     # out = nn.functional.tanh(self.tanh_linear(x))
 
-    # tanh = nn.functional.tanh(self.tanh_linear(x))
-    # sigmoid = nn.functional.sigmoid(self.sigmoid_linear(x))
-    # out = tanh * sigmoid
+    tanh = self.tanh_linear(x)
+    sigmoid = nn.functional.sigmoid(self.sigmoid_linear(x))
+    out = tanh * sigmoid
     return out
