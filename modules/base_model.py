b4727fa798ab45608b1d9ff98ecd61f6e6d27ce7
hengyuan-hu <kevin.huhy@gmail.com>
2017-10-27 15:16:23 -0400

    simplify naming.

--- modules/base_model.py
+++ modules/base_model.py
@@ -8,10 +8,10 @@ from ram import RAM
 
 
 class BaseModel(nn.Module):
-    def __init__(self, q_emb, v_attention, q_net, v_net, classifier):
+    def __init__(self, q_emb, v_att, q_net, v_net, classifier):
         super(BaseModel, self).__init__()
         self.q_emb = q_emb
-        self.v_attention = v_attention
+        self.v_att = v_att
         self.q_net = q_net
         self.v_net = v_net
         self.classifier = classifier
@@ -35,7 +35,7 @@ class BaseModel(nn.Module):
         q_emb = self.q_emb(q) # [batch, q_dim]
         q_repr = self.q_net(q_emb)
 
-        v_emb = self.v_attention(v, q_emb).sum(1) # [batch, v_dim]
+        v_emb = self.v_att(v, q_emb).sum(1) # [batch, v_dim]
         v_repr = self.v_net(v_emb)
         joint_repr = q_repr * v_repr
         return joint_repr
@@ -43,16 +43,16 @@ class BaseModel(nn.Module):
 
 def build_baseline0(dataset, num_hid):
     q_emb = QuestionEmbedding(dataset.dictionary.ntoken, 300, num_hid, 1, False)
-    v_attention = TopDownAttention(q_emb.nhid, dataset.v_dim, num_hid)
+    v_att = TopDownAttention(q_emb.nhid, dataset.v_dim, num_hid)
     q_net = GLU(q_emb.nhid, num_hid)
     v_net = GLU(dataset.v_dim, num_hid)
     classifier = SimpleClassifier(num_hid, num_hid * 2, dataset.num_ans_candidates)
-    return BaseModel(q_emb, v_attention, q_net, v_net, classifier)
+    return BaseModel(q_emb, v_att, q_net, v_net, classifier)
 
 
 def build_baseline1(dataset, num_hid):
     q_emb = QuestionEmbedding(dataset.dictionary.ntoken, 300, num_hid, 1, False)
-    v_attention = TopDownAttention(q_emb.nhid, dataset.v_dim, num_hid)
+    v_att = TopDownAttention(q_emb.nhid, dataset.v_dim, num_hid)
     q_net = GLU(q_emb.nhid, num_hid)
 
     v_net = nn.Sequential(
@@ -60,17 +60,17 @@ def build_baseline1(dataset, num_hid):
         GLU(dataset.v_dim, num_hid)
     )
     classifier = SimpleClassifier(num_hid, num_hid * 2, dataset.num_ans_candidates)
-    return BaseModel(q_emb, v_attention, q_net, v_net, classifier)
+    return BaseModel(q_emb, v_att, q_net, v_net, classifier)
 
 
 # Not very useful
 def build_baseline0_bidirect(dataset, num_hid):
     q_emb = QuestionEmbedding(dataset.dictionary.ntoken, 300, num_hid, 1, True)
-    v_attention = TopDownAttention(q_emb.nhid * 2, dataset.v_dim, num_hid)
+    v_att = TopDownAttention(q_emb.nhid * 2, dataset.v_dim, num_hid)
     q_net = GLU(q_emb.nhid * 2, num_hid)
     v_net = GLU(dataset.v_dim, num_hid)
     classifier = SimpleClassifier(num_hid, num_hid * 2, dataset.num_ans_candidates)
-    return BaseModel(q_emb, v_attention, q_net, v_net, classifier)
+    return BaseModel(q_emb, v_att, q_net, v_net, classifier)
 
 
 def build_ram0(dataset, num_hid):
@@ -82,8 +82,8 @@ def build_ram0(dataset, num_hid):
         GLU(rnet_in_dim, dataset.v_dim),
         # GLU(dataset.v_dim, dataset.v_dim)
     )
-    v_attention = TopDownAttention(q_emb.nhid, dataset.v_dim, num_hid)
-    ram = RAM(v_attention, r_net)
+    v_att = TopDownAttention(q_emb.nhid, dataset.v_dim, num_hid)
+    ram = RAM(v_att, r_net)
 
     v_net = GLU(dataset.v_dim, num_hid)
     classifier = SimpleClassifier(num_hid, num_hid * 2, dataset.num_ans_candidates)
