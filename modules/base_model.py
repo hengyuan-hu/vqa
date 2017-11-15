21a95efdb9d5638e36277802e8385ec5c43d9aed
hengyuan-hu <kevin.huhy@gmail.com>
2017-11-15 18:38:36 -0500

    det baseline.

--- modules/base_model.py
+++ modules/base_model.py
@@ -39,6 +39,45 @@ class BaseModel(nn.Module):
         return logits
 
 
+class DetBaseModel(nn.Module):
+    def __init__(self, w_emb, q_emb, v_att, det_att, q_net, v_net, classifier):
+        super(DetBaseModel, self).__init__()
+        self.w_emb = w_emb
+        self.q_emb = q_emb
+        self.v_att = v_att
+        self.det_att = det_att
+        self.q_net = q_net
+        self.v_net = v_net
+        self.classifier = classifier
+
+    def forward(self, v, b, det, q, labels):
+        """Forward
+
+        v: [batch, num_objs, obj_dim]
+        b: [batch, num_objs, b_dim]
+        q: [batch_size, seq_length]
+
+        return: logits, not probs
+        """
+        w_emb = self.w_emb(q)
+        q_emb = self.q_emb(w_emb) # [batch, q_dim]
+        q_repr = self.q_net(q_emb)
+
+        # v = torch.cat([v, b], 2)
+        v_att = self.v_att.logits(v, q_emb)#.unsqueeze(2).expand_as(v)
+        det_emb = self.w_emb(det) # [batch, num_objs, w_dim]
+        det_att = self.det_att.logits(det_emb, q_emb)
+        att_weights = nn.functional.softmax(det_att + v_att).unsqueeze(2)
+
+        v_emb = (att_weights * v).sum(1) # [batch, v_dim]
+        v_repr = self.v_net(v_emb)
+
+        joint_repr = q_repr * v_repr
+        logits = self.classifier(joint_repr)
+        return logits
+
+
+
 def build_baseline0(dataset, num_hid):
     w_emb = WordEmbedding(dataset.dictionary.ntoken, 300, 0.0)
     q_emb = QuestionEmbedding(300, num_hid, 1, False, 0.0)
@@ -61,3 +100,15 @@ def build_baseline0_newatt(dataset, num_hid):
     # v_net = GLU(dataset.v_dim + dataset.s_dim, num_hid)
     classifier = SimpleClassifier(num_hid, num_hid * 2, dataset.num_ans_candidates)
     return BaseModel(w_emb, q_emb, v_att, q_net, v_net, classifier)
+
+
+def build_det_baseline0(dataset, num_hid):
+    w_emb = WordEmbedding(dataset.dictionary.ntoken, 300, 0.0)
+    q_emb = QuestionEmbedding(300, num_hid, 1, False, 0.0)
+    v_att = NewAttention(dataset.v_dim, q_emb.num_hid)
+    det_att = TopDownAttention(q_emb.num_hid, 300, num_hid)
+    # det_att = NewAttention(300, q_emb.num_hid)
+    q_net = GLU(q_emb.num_hid, num_hid)
+    v_net = GLU(dataset.v_dim, num_hid)
+    classifier = SimpleClassifier(num_hid, num_hid * 2, dataset.num_ans_candidates)
+    return DetBaseModel(w_emb, q_emb, v_att, det_att, q_net, v_net, classifier)
