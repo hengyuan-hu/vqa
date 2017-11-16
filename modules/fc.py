0245ffd755082e936ead131b948d88d8454a8751
Hengyuan Hu <kevin.huhy@gmail.com>
2017-11-16 16:17:05 -0500

    correct fc net?

--- modules/fc.py
+++ modules/fc.py
@@ -7,15 +7,25 @@ class FCNet(nn.Module):
         super(FCNet, self).__init__()
 
         layers = []
-        for i in range(len(dims)-1):
+        for i in range(len(dims)-2):
             in_dim = dims[i]
             out_dim = dims[i+1]
             layers.append(weight_norm(nn.Linear(in_dim, out_dim), dim=None))
             layers.append(nn.ReLU())
-        if dropout:
+        if len(dims) > 2 and dropout:
             layers.append(nn.Dropout(0.5))
+        layers.append(weight_norm(nn.Linear(dims[-2], dims[-1]), dim=None))
 
         self.main = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.main(x)
+
+
+if __name__ == '__main__':
+    fc1 = FCNet([10, 20, 10], 0.5)
+    print fc1
+
+    print '============'
+    fc2 = FCNet([10, 20], 0.5)
+    print fc2
