d43635bf1b2d90cdff66df7c3c421ba9e3169236
hengyuan-hu <kevin.huhy@gmail.com>
2017-11-17 18:14:32 -0500

    two word embeddings.

--- modules/relation_model.py
+++ modules/relation_model.py
@@ -52,6 +52,52 @@ class RelationModel0(nn.Module):
         return logits
 
 
+class RelationModel1(nn.Module):
+    def __init__(self, w_emb, q_emb, v_att, relation, q_net, v_net, classifier):
+        super(RelationModel1, self).__init__()
+        self.w_emb = w_emb
+        self.q_emb = q_emb
+        self.v_att = v_att
+        self.q_net = q_net
+        self.v_net = v_net
+        self.classifier = classifier
+
+        self.relation = relation
+
+        self.w_sem_att = UniAttention(self.q_emb.num_hid * self.q_emb.ndirections)
+        self.w_rel_att = UniAttention(self.q_emb.num_hid * self.q_emb.ndirections)
+
+    def forward(self, v, b, det, q, labels):
+        """Forward
+
+        v: [batch, num_objs, obj_dim]
+        b: [batch, num_objs, b_dim]
+        q: [batch_size, seq_length]
+
+        return: logits, not probs
+        """
+        w_emb = self.w_emb(q)
+        q_emb = self.q_emb.forward_all(w_emb)
+        q_repr = self.q_net(q_emb[:, -1])
+
+        w_sem_att = self.w_sem_att(q_emb).unsqueeze(2)
+        w_sem_emb = (w_emb * w_sem_att).sum(1)
+        w_rel_att = self.w_rel_att(q_emb).unsqueeze(2)
+        w_rel_emb = (w_emb * w_rel_att).sum(1)
+
+        v_att = self.v_att(v, w_sem_emb).unsqueeze(2) # [batch, k, 1]
+        b = b[:, :, :4] # area is not used
+        relation_matrix = self.relation(b, w_rel_emb) # [batch, k, k]
+        prop_att = torch.bmm(relation_matrix, v_att) # [batch, k, 1]
+
+        rv_emb = (prop_att * v).sum(1)
+        rv_repr = self.v_net(rv_emb)
+        joint_repr = q_repr * rv_repr
+
+        # joint_repr = q_repr * (v_repr + rv_repr)
+        logits = self.classifier(joint_repr)
+        return logits
+
 
 def build_rm0(dataset, num_hid):
     w_emb = WordEmbedding(dataset.dictionary.ntoken, 300, 0.0)
@@ -62,5 +108,16 @@ def build_rm0(dataset, num_hid):
     q_net = FCNet([q_emb.num_hid, num_hid], 0)
     v_net = FCNet([dataset.v_dim, num_hid], 0)
     classifier = SimpleClassifier(num_hid, num_hid * 2, dataset.num_ans_candidates)
-
     return RelationModel0(w_emb, q_emb, v_att, relation, q_net, v_net, classifier)
+
+
+def build_rm1(dataset, num_hid):
+    w_emb = WordEmbedding(dataset.dictionary.ntoken, 300, 0.0)
+    q_emb = QuestionEmbedding(300, num_hid, 1, False, 0.0)
+
+    v_att = NewAttention2(dataset.v_dim, 300, num_hid)
+    relation = RelationModule(None, 300, None)
+    q_net = FCNet([q_emb.num_hid, num_hid], 0)
+    v_net = FCNet([dataset.v_dim, num_hid], 0)
+    classifier = SimpleClassifier(num_hid, num_hid * 2, dataset.num_ans_candidates)
+    return RelationModel1(w_emb, q_emb, v_att, relation, q_net, v_net, classifier)
