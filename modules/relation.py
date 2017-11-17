590d1d6fdda58c0a2eaf2b086ee9e6fbd9b40ed3
hengyuan-hu <kevin.huhy@gmail.com>
2017-11-17 01:58:43 -0500

    relation comeback

--- modules/relation.py
+++ modules/relation.py
@@ -19,12 +19,22 @@ def softmax(x, dim):
 
 
 class RelationModule(nn.Module):
-    def __init__(self, v_dim, q_dim, num_hid):
+    def __init__(self, s_dim, q_dim, num_hid):
         super(RelationModule, self).__init__()
 
-        self.v_proj = weight_norm(nn.Linear(v_dim * 2, num_hid), dim=None)
-        self.q_proj = weight_norm(nn.Linear(q_dim, num_hid), dim=None)
-        self.linear = weight_norm(nn.Linear(num_hid, 1), dim=None)
+        self.v_conv = nn.Sequential(
+            weight_norm(nn.Linear(2, 32), dim=None),
+            nn.ReLU())
+        self.v_proj = nn.Sequential(
+            weight_norm(nn.Linear(128, 128), dim=None),
+            nn.ReLU())
+
+        self.q_proj = nn.Sequential(
+            weight_norm(nn.Linear(q_dim, 128), dim=None),
+            nn.ReLU())
+
+        self.dropout = nn.Dropout(0.5)
+        self.linear = weight_norm(nn.Linear(128, 1), dim=None)
 
     def forward(self, v, q):
         """
@@ -35,25 +45,32 @@ class RelationModule(nn.Module):
 
         vi = v.unsqueeze(1).repeat(1, k, 1, 1)
         vj = v.unsqueeze(2).repeat(1, 1, k, 1)
-        v_cat = torch.cat([vi, vj], 3)
-        v_proj = self.v_proj(v_cat) # [batch, k, k, hid]
+        v_stack = torch.stack([vi, vj], 4) # [batch, k, k, 4, 2]
+        # print v_stack.size()
+        v_conv = self.v_conv(v_stack) # [batch, k, k, 4, 32]
+        v_conv = v_conv.view(batch, k, k, -1) # [batch, k, k, 128]
+        # print v_conv.size()
+        v_proj = self.v_proj(v_conv) # [batch, k, k, hid]
+
         q_proj = self.q_proj(q) # [batch, hid]
         q_proj = q_proj.unsqueeze(1).unsqueeze(1).expand_as(v_proj)
         # q_proj [batch, k, k, hid]
         # assert q_proj.size() == q_proj.size()
 
         joint_repr = v_proj * q_proj
-        joint_repr = nn.functional.normalize(joint_repr, 2, 3)
+        joint_repr = self.dropout(joint_repr)
+        # print joint_repr.size()
+        # joint_repr = nn.functional.normalize(joint_repr, 2, 3)
         logits = self.linear(joint_repr).squeeze(3) # [batch, k, k]
         w = softmax(logits, 1)
         return w
 
 
 if __name__ == '__main__':
-    rm = RelationModule(3, 3, 2)
+    rm = RelationModule(4, 5, 2)
 
-    v = Variable(torch.rand(2, 3, 3))
-    q = Variable(torch.rand(2, 3))
+    v = Variable(torch.rand(2, 3, 4))
+    q = Variable(torch.rand(2, 5))
 
     w = rm(v, q)
 
--- modules/relation_model.py
+++ modules/relation_model.py
@@ -2,77 +2,27 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from top_down_attention import TopDownAttention
-from attention import NewAttention, UniAttention
+from attention import NewAttention2, UniAttention
 from language_model import WordEmbedding, QuestionEmbedding
 # from glu import GLU
 from classifier import SimpleClassifier
 from relation import RelationModule
+from fc import FCNet
 
 
 class RelationModel0(nn.Module):
-    def __init__(self, q_emb,
-                 q_sementic_att, q_relation_att,
-                 v_att, relation, q_net, v_net, classifier):
+    def __init__(self, w_emb, q_emb, v_att, relation, q_net, v_net, classifier):
         super(RelationModel0, self).__init__()
-        self.q_emb = q_emb
-        self.q_sementic_att = q_sementic_att
-        self.q_relation_att = q_relation_att
-
-        self.v_att = v_att
-        self.relation = relation
-
-        self.q_net = q_net
-        self.v_net = v_net
-        self.classifier = classifier
-
-    def forward(self, v, b, q, labels):
-        """Forward
-
-        v: [batch, num_objs, obj_dim]
-        b: [batch, num_objs, b_dim]
-        q: [batch_size, seq_length]
-
-        return: logits, not probs
-        """
-        w_emb, q_annotations = self.q_emb.forward_allout(q) # [batch, sequence, q_dim]
-        # [batch, sequence, 1]
-        q_sementic_att = self.q_sementic_att(q_annotations).unsqueeze(2)
-        q_relation_att = self.q_relation_att(q_annotations).unsqueeze(2)
-        q_sementic_emb = (w_emb * q_sementic_att).sum(1)
-        q_relation_emb = (w_emb * q_relation_att).sum(1)
-        q_joint_emb = torch.cat([q_sementic_emb, q_relation_emb], 1)
-        q_repr = self.q_net(q_joint_emb)
-
-        sementic_att = self.v_att(v, q_sementic_emb).unsqueeze(2) #[batch, k, 1]
-        relation_att = self.relation(b, q_relation_emb) # [batch, k, k]
-        prop_att = torch.bmm(relation_att, sementic_att) # [batch, k, 1]
-        v_emb = (sementic_att * v).sum(1) # [batch, v_dim]
-        v_repr = self.v_net(v_emb)
-
-        rv_emb = (prop_att * v).sum(1)
-        rv_repr = self.v_net(rv_emb)
-
-        joint_repr = q_repr * (v_repr + rv_repr)
-        logits = self.classifier(joint_repr)
-        return logits
-
-
-class RelationModel1(nn.Module):
-    def __init__(self, w_emb, q_sementic_emb, q_relation_emb,
-                 v_att, relation, q_net, v_net, classifier):
-        super(RelationModel1, self).__init__()
         self.w_emb = w_emb
-        self.q_sementic_emb = q_sementic_emb
-        self.q_relation_emb = q_relation_emb
-
+        self.q_emb = q_emb
         self.v_att = v_att
-        self.relation = relation
-
         self.q_net = q_net
         self.v_net = v_net
         self.classifier = classifier
 
-    def forward(self, v, b, q, labels):
+        self.relation = relation
+
+    def forward(self, v, b, det, q, labels):
         """Forward
 
         v: [batch, num_objs, obj_dim]
@@ -82,52 +32,35 @@ class RelationModel1(nn.Module):
         return: logits, not probs
         """
         w_emb = self.w_emb(q)
-        q_sementic_emb = self.q_sementic_emb(w_emb) # [batch, q_dim]
-        q_relation_emb = self.q_relation_emb(w_emb) # [batch, q_dim]
+        q_emb = self.q_emb(w_emb)
+        q_repr = self.q_net(q_emb)
 
-        # q_joint_emb = torch.cat([q_sementic_emb, q_relation_emb], 1)
-        q_sementic_repr = self.q_net(q_sementic_emb)
-        q_relation_repr = self.q_net(q_relation_emb)
+        v_att = self.v_att(v, q_emb).unsqueeze(2) # [batch, k, 1]
+        b = b[:, :, :4] # area is not used
+        relation_matrix = self.relation(b, q_emb) # [batch, k, k]
+        prop_att = torch.bmm(relation_matrix, v_att) # [batch, k, 1]
 
-        sementic_att = self.v_att(v, q_sementic_emb).unsqueeze(2) #[batch, k, 1]
-        relation_att = self.relation(b, q_relation_emb) # [batch, k, k]
-        prop_att = torch.bmm(relation_att, sementic_att) # [batch, k, 1]
-        v_emb = (sementic_att * v).sum(1) # [batch, v_dim]
-        v_repr = self.v_net(v_emb)
+        # v_emb = (sementic_att * v).sum(1) # [batch, v_dim]
+        # v_repr = self.v_net(v_emb)
 
         rv_emb = (prop_att * v).sum(1)
         rv_repr = self.v_net(rv_emb)
+        joint_repr = q_repr * rv_repr
 
-        joint_repr = q_sementic_repr * v_repr + q_relation_repr * rv_repr
+        # joint_repr = q_repr * (v_repr + rv_repr)
         logits = self.classifier(joint_repr)
         return logits
 
 
+
 def build_rm0(dataset, num_hid):
-    q_emb = QuestionEmbedding(dataset.dictionary.ntoken, 300, num_hid, 1, False)
-    q_att1 = UniAttention(q_emb.nhid)
-    q_att2 = UniAttention(q_emb.nhid)
-
-    v_att = NewAttention(dataset.v_dim, 300)
-    relation = RelationModule(dataset.s_dim, 300, 128)
-    q_net = GLU(600, num_hid)
-    v_net = GLU(dataset.v_dim, num_hid)
-    classifier = SimpleClassifier(num_hid, num_hid * 2, dataset.num_ans_candidates)
-
-    return RelationModel0(
-        q_emb, q_att1, q_att2, v_att, relation, q_net, v_net, classifier)
-
-
-def build_rm1(dataset, num_hid):
     w_emb = WordEmbedding(dataset.dictionary.ntoken, 300, 0.0)
-    q_emb1 = QuestionEmbedding(300, num_hid, 1, False, 0.0)
-    q_emb2 = QuestionEmbedding(300, num_hid, 1, False, 0.0)
+    q_emb = QuestionEmbedding(300, num_hid, 1, False, 0.0)
 
-    v_att = NewAttention(dataset.v_dim, num_hid)
-    relation = RelationModule(dataset.s_dim, num_hid, 128)
-    q_net = GLU(num_hid, num_hid)
-    v_net = GLU(dataset.v_dim, num_hid)
+    v_att = NewAttention2(dataset.v_dim, q_emb.num_hid, num_hid)
+    relation = RelationModule(None, q_emb.num_hid, None)
+    q_net = FCNet([q_emb.num_hid, num_hid], 0)
+    v_net = FCNet([dataset.v_dim, num_hid], 0)
     classifier = SimpleClassifier(num_hid, num_hid * 2, dataset.num_ans_candidates)
 
-    return RelationModel1(
-        w_emb, q_emb1, q_emb2, v_att, relation, q_net, v_net, classifier)
+    return RelationModel0(w_emb, q_emb, v_att, relation, q_net, v_net, classifier)
